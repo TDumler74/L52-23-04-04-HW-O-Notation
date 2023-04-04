2e97219c691e8b3cba15893172acb88519ddfc6f
TDumler74 <tatjana.dumler@gmail.com>
2023-04-04 15:58:20 +0200

    first

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        /*
+1.f(n) = 9n +73                     f(n)=n;
+2.f(n) = 235                        f(n)=1
+3.f(n) = n^2 + 7n +123              f(n)=n^2
+4.f(n) = n^3 +2000n +8765           f(n)=n^3
+5.f(n) = n + sqrt(n)                f(n)=n
+         */
+    }
+}
