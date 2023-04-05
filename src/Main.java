569b07b39e1afebdf2a0b2a3ba91f90d5dbda4a3
TDumler74 <tatjana.dumler@gmail.com>
2023-04-05 09:37:13 +0200

    first

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
 
         /*
-1.f(n) = 9n +73                     f(n)=n      O(n)
-2.f(n) = 235                        f(n)=1      O(1)
-3.f(n) = n^2 + 7n +123              f(n)=n^2    O(n^2)
-4.f(n) = n^3 +2000n +8765           f(n)=n^3    O(n^3)
-5.f(n) = n + sqrt(n)wurzel von n    f(n)=n      O(n)
+1.f(n) = 9n +73                       f(n)=n      O(n)
+2.f(n) = 235                          f(n)=1      O(1)
+3.f(n) = n^2 + 7n +123                f(n)=n^2    O(n^2)
+4.f(n) = n^3 +2000n +8765             f(n)=n^3    O(n^3)
+5.f(n) = n + sqrt(n)//wurzel von n    f(n)=n      O(n)
          */
     }
 }
