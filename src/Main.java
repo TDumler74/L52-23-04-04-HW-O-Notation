0334585a060cc092fe502a60e8ec989b31ccb59f
TDumler74 <tatjana.dumler@gmail.com>
2023-04-06 09:40:10 +0200

    binarySearche

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,37 @@ public class Main {
     public static void main(String[] args) {
 
 
-
-        /*
-1.f(n) = 9n +73                       f(n)=n      O(n)
-2.f(n) = 235                          f(n)=1      O(1)
-3.f(n) = n^2 + 7n +123                f(n)=n^2    O(n^2)
-4.f(n) = n^3 +2000n +8765             f(n)=n^3    O(n^3)
-5.f(n) = n + sqrt(n)//wurzel von n    f(n)=n      O(n)
-         */
+        int[] ints = {1, 3, 5, 9, 13, 16, 19, 22, 33, 48, 56};
+        System.out.println(binarSuche(1, ints));
+        System.out.println(binarSuche(19, ints));
+        System.out.println(binarSuche(16, ints));
+        System.out.println(binarSuche(13, ints));
+        System.out.println(binarSuche(48, ints));
+        System.out.println(binarSuche(56, ints));
     }
+
+
+    public static int binarSuche(int zahl, int[] arr) {
+        int anfang = 0;
+        int ende = arr.length - 1;
+
+        while (anfang <= ende) {
+            int mitte = (anfang + ende) / 2;
+            if (zahl < arr[mitte]) {
+                ende = mitte - 1;
+            }
+            if (zahl > arr[mitte]) {
+                anfang = mitte + 1;
+            }
+            if (zahl == arr[mitte]) {
+                return mitte;
+            }
+        }
+        return -1;
+    }
+
+
 }
+
+
+
